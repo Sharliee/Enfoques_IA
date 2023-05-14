c099d5828fe231cd3dcbd506018a01fa8ff7c7e8
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-13 23:14:55 -0600

    Update Propagacion_De_Restricciones.py

--- 003_Satisfaccion_De_Restriccion/Propagacion_De_Restricciones.py
+++ 003_Satisfaccion_De_Restriccion/Propagacion_De_Restricciones.py
@@ -33,13 +33,3 @@ soluciones = problem.getSolutions()
 print("Se encontraron", len(soluciones), "soluciones:")
 for solucion in soluciones:
     print(solucion)
-#En este ejemplo, se importa la biblioteca de propagación de restricciones de 
-#Python y se crea un objeto de problema.
-# Se agregan las 15 variables correspondientes a cada nombre a la lista de variables del problema y se les asigna el dominio de posibles trabajos (1, 2 o 3).
-#Luego se agregan las restricciones correspondientes a las relaciones entre los
-# nombres. Por ejemplo, la restricción "Juan, Pedro y Luis no pueden tener el mismo
-# trabajo" se agrega asegurando que los valores asignados a estas tres variables 
-#sean distintos (constraint.AllDifferentConstraint())
-#Finalmente, se resuelve el problema y se imprimen todas las soluciones 
-#encontradas. En este caso, cada solución correspondería a una asignación de 
-#trabajos a las 15 personas que cumple con todas las restricciones agregadas.
