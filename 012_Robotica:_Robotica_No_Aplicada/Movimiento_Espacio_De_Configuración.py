842e16f2c182bfab0e8592032f5a6d60fd05539e
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-14 08:25:31 -0600

    Update Movimiento_Espacio_De_Configuración.py

--- 012_Robotica:_Robotica_No_Aplicada/Movimiento_Espacio_De_Configuración.py
+++ 012_Robotica:_Robotica_No_Aplicada/Movimiento_Espacio_De_Configuración.py
@@ -1,20 +1,3 @@
-"""
-En la robótica, el espacio de configuración se refiere al conjunto de todas las posibles configuraciones o 
-estados que puede tomar un robot. En otras palabras, el espacio de configuración es el espacio matemático 
-que describe todas las posibles posiciones, orientaciones y velocidades de las articulaciones de un robot.
-
-El espacio de configuración es un concepto importante en la planificación del movimiento de los robots, ya 
-que permite modelar y analizar el movimiento de los robots en un espacio matemático abstracto. Por ejemplo, 
-si se sabe que un robot tiene seis articulaciones, cada una de las cuales puede girar en un rango de 0 a 360 
-grados, el espacio de configuración del robot sería un espacio de seis dimensiones que describe todas las 
-posibles combinaciones de ángulos de articulación.
-
-
-La planificación del movimiento en el espacio de configuración permite a los programadores y diseñadores de 
-robots definir trayectorias de movimiento para el robot, evitando obstáculos y optimizando el tiempo de movimiento. 
-En general, el espacio de configuración es una herramienta útil para modelar y entender el comportamiento de los 
-robots en una variedad de contextos.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
