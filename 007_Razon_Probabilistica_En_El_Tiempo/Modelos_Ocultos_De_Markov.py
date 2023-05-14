fdad811b7867023ee432578a85044593c8016925
Sharlo RDT <124947231+Sharliee@users.noreply.github.com>
2023-05-14 00:09:51 -0600

    Update Modelos_Ocultos_De_Markov.py

--- 007_Razon_Probabilistica_En_El_Tiempo/Modelos_Ocultos_De_Markov.py
+++ 007_Razon_Probabilistica_En_El_Tiempo/Modelos_Ocultos_De_Markov.py
@@ -1,27 +1,3 @@
-#Modelo oculto de Markov
-"""
-El modelo oculto de Markov (HMM, por sus siglas en inglés) es un
-modelo estadístico que se utiliza para modelar secuencias de
-eventos, en los que el estado subyacente no es observable
-directamente, pero se puede inferir a través de observaciones.
-Es decir, el modelo asume que el estado de un sistema se puede 
-representar mediante una cadena de Markov, pero en lugar de 
-observar directamente el estado, solo se observa una serie de 
-símbolos asociados con cada estado.
-
-En la programación, los HMM se utilizan en una variedad 
-de aplicaciones, como el reconocimiento de voz, la detección de
-spam, la predicción del clima, el análisis de sentimientos y el 
-procesamiento del lenguaje natural. Los HMM se implementan a 
-menudo utilizando bibliotecas especializadas, como hmmlearn en 
-Python, que proporcionan una interfaz para entrenar y utilizar 
-modelos HMM en conjuntos de datos específicos. La implementación
-típica implica el uso de algoritmos de aprendizaje para estimar
-los parámetros del modelo, como las probabilidades de transición
-y emisión, y luego utilizar el modelo para predecir el estado 
-subyacente a partir de las observaciones dadas.
-"""
-
 # Importar módulo de HMM
 from hmmlearn import hmm
 
@@ -51,21 +27,3 @@ secuencia_generada, estados = modelo.sample(n_samples=5)
 # Mostrar la secuencia generada y los estados del modelo
 print("Secuencia generada:", secuencia_generada)
 print("Estados del modelo:", estados)
-
-#Los estados ocultos son en un HMM son las variables 
-#internas del modelo que influyen en las observaciones 
-#que se hacen, pero que no se pueden observar directamente.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
